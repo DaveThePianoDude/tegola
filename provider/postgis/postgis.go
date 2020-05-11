4feef1a88f0d5ed3440d33ef318fadce0a16ee7a
Joe Meilinger <joemeilinger@gmail.com>
2020-05-11 09:42:23 -0700

    Use proj library
    Make use of the go-spatial/proj library for Projection Support.

--- provider/postgis/postgis.go
+++ provider/postgis/postgis.go
@@ -10,13 +10,12 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/jackc/pgx"
-
 	"github.com/go-spatial/geom"
 	"github.com/go-spatial/geom/encoding/wkb"
 	"github.com/go-spatial/tegola/dict"
 	"github.com/go-spatial/tegola/proj"
 	"github.com/go-spatial/tegola/provider"
+	"github.com/jackc/pgx"
 )
 
 const Name = "postgis"
@@ -41,7 +40,7 @@ const (
 
 const (
 	DefaultPort    = 5432
-	DefaultSRID    = proj.WebMercator
+	DefaultSRID    = proj.WebMercatorSRID
 	DefaultMaxConn = 100
 	DefaultSSLMode = "disable"
 	DefaultSSLKey  = ""
@@ -431,7 +430,7 @@ func (p Provider) inspectLayerGeomType(l *Layer) error {
 	sql = strings.Replace(sql, "!ZOOM!", "ANY('{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}')", 1)
 
 	// we need a tile to run our sql through the replacer
-	tile := provider.NewTile(0, 0, 0, 64, proj.WebMercator)
+	tile := provider.NewTile(0, 0, 0, 64, proj.WebMercatorSRID)
 
 	// normal replacer
 	sql, err = replaceTokens(sql, l.srid, tile)
